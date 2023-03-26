902b701f255d582cafa38c6edb6753ebd842bd68
Evan Haveman <evanhaveman@gmail.com>
2023-03-26 15:39:44 -0700

    removed package dependencies

--- functions/getrelatedconcepts.js
+++ functions/getrelatedconcepts.js
@@ -1,21 +1,6 @@
-import { Configuration, OpenAIApi } from "openai-edge"
-
-const prompt = (concept) => `
-    I am building a concept map. Provide 5 related concepts and their relationships to "${concept}".
-    Encode your response in this json format:
-    { "concept": "${concept}", "connections": [ { "concept": "", "relationship": "" } ] }
-`.trim();
-
 export async function onRequest(context) {
     const request = context.request;
     const body = await request.json();
-
-    console.log(context.env.OPENAI_API_KEY);
-
-    const configuration = new Configuration({
-        apiKey: context.env.OPENAI_API_KEY
-    });
-    const openai = new OpenAIApi(configuration)
 
     const { concept } = body;
 
@@ -26,16 +11,27 @@ export async function onRequest(context) {
 
     while (!json && i < 3) {
         console.log(`attempt ${i}`);
-        const response = await (await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: prompt(concept),
-            max_tokens: 1024,
-            temperature: 0.7
+
+        const response = await (await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${context.env.OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    { role: 'system', content: 'You are a librarian with ancyclopaedic knowledge of all concepts and their relationships. The user will give you a concept and ask for related concepts in return. Your response should be formatted like { "concept": "${concept}", "connections": [ { "concept": "", "relationship": "" } ] }' },
+                    { role: 'user', content: 'Concepts related to: consciousness' },
+                    { role: 'assistant', content: '{ "concetp": "consciousness", "connections": [{ "concept": "qulia", "relationship": "comprises" }, { "concept": "cognition", "relationship": "involves" }, { "concept": "perception", "relationship": "integrates" }, { "concept": "self-identity", "relationship": "constructs" }, { "concept": "awareness", "relationship": "encompasses" } ] }' },
+                    { role: 'user', content: `What are concepts related to ${concept}?` }
+                ]
+            })
         })).json();
 
         console.log(response);
 
-        const text = response.choices[0].text;
+        const text = response.choices[0].message.content;
 
         try {
             const a = text.indexOf('{');
